46240cec01a6dbe2d0a7a9011f7116b7081427a7
Gabriel Perdue <perdue@fnal.gov>
2016-07-15 10:17:33 -0500

    add group

--- kill_held_jobs.sh
+++ kill_held_jobs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jobsub_rm --role=Analysis  --jobid="`jobsub_q --user perdue | grep "GaudiAna" | grep "H" | cut -d ' ' -f 1 | tr '\n' ','`"
+jobsub_rm -G minerva --role=Analysis  --jobid="`jobsub_q --user perdue | grep "GaudiAna" | grep "H" | cut -d ' ' -f 1 | tr '\n' ','`"
